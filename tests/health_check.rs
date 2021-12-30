36ce4757a1d34a6b8d73d2c9af19cb2ac84da7ba
Alexander Kalinin <alex@kalinin.uk>
2021-12-30 17:03:27 +0000

    update config for deployment and ssl db

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -116,14 +116,14 @@ async fn spawn_app() -> TestApp {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+    let mut connection = PgConnection::connect_with(&config.without_db())
         .await
         .expect("Failed to connect to postgres");
     connection
         .execute(format!(r#"create database "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database");
-    let db_pool = PgPool::connect(&config.connection_string())
+    let db_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to postgres");
     sqlx::migrate!("./migrations")
